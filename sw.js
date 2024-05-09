841053e37ba7ed9381b37bc259ec1e4dbf2a240e
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2024-05-09 20:12:44 -0300

    Pasos1 Updated

--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
 
+self.addEventListener('fetch',event=>{
 
+    const offLineResponse = new Response(`
+        
+        Bienvenido a mi sitio web.
 
+        Disculpa , pero para usarla, necesitas internet.
+        
+    `)
+
+    const resp = fetch(event.request)
+                    .catch( ()=> offLineResponse )
+
+    event.respondWith( resp )
+}) 
