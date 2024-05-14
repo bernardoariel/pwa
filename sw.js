1a358bc49a216aa6812bf0178d5383f51a44e226
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2024-05-14 10:29:23 -0300

    Agregando el AppShell

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
-
-self.addEventListener('fetch',event=>{
-
-    const offLineResponse = new Response(fetch('pages/offline.html'))
-
-    const resp = fetch(event.request)
-                    .catch( ()=> offLineResponse )
-
-    event.respondWith( resp )
-}) 
+self.addEventListener('install', e => {
+    const cacheProm = caches.open('cache-1').then(cache => {
+        return cache.addAll([
+            '/index.html',
+            '/css/style.css',
+            '/img/image.png',
+            '/js/app.js',
+            'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+            '/pages/offline.html'
+        ]).catch(error => {
+            console.error('Failed to cache:', error);
+            throw error;
+        });
+    });
+    e.waitUntil(cacheProm);
+});
