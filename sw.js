9c118c1cefc1eec303f88e45d374adceebc3fb3e
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2024-05-21 10:35:51 -0300

    Fix al cargar las imagenes dinamicas

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // const CACHE_NAME = 'cache-1'
 
-const CACHE_STATIC_NAME = 'static-v1'
+const CACHE_STATIC_NAME = 'static-v2'
 const CACHE_DYNAMIC_NAME = 'dynamic-v1'
 
 const CACHE_INMUTABLE_NAME = 'inmutable-v1'
