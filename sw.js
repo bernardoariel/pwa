a90e573d0ca30a1356fe7102dc99bad88b732a04
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2024-05-20 21:46:43 -0300

    Clase Cache with network fallback

--- sw.js
+++ sw.js
@@ -1,28 +1,21 @@
-// const CACHE_NAME = 'cache-1'
+const CACHE_NAME = 'cache-1'
 
-const CACHE_STATIC_NAME = 'static-v1'
-const CACHE_DYNAMIC_NAME = 'dynamic-v1'
 
-const CACHE_INMUTABLE_NAME = 'inmutable-v1'
 
 self.addEventListener('install', e => {
-    const cacheProm = caches.open(CACHE_STATIC_NAME).then(cache => {
+    const cacheProm = caches.open(CACHE_NAME).then(cache => {
         return cache.addAll([
             '/',
             '/index.html',
             '/css/style.css',
             '/img/image.png',
             '/js/app.js',
-            '/pages/offline.html'
-        ]).catch(error => {
-            console.error('Failed to cache:', error);
-            throw error;
-        });
+            '/pages/offline.html',
+            'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
+        ])
     });
-    const cacheInputable = caches.open(CACHE_INMUTABLE_NAME).then(cache => {
-      return cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css')
-    });
-    e.waitUntil(Promise.all([cacheProm,cacheInputable]));
+   
+    e.waitUntil(cacheProm);
 });
 
 self.addEventListener('fetch',e=>{
@@ -36,12 +29,12 @@ self.addEventListener('fetch',e=>{
 
         // No existe el archivo
         // tengo que ir a la web
-      
+        
         console.log('No existe', e.request)
         
         return fetch(e.request)
           .then(newResponse =>{
-            caches.open(CACHE_DYNAMIC_NAME).then( cache =>{
+            caches.open(CACHE_NAME).then( cache =>{
               if(cache){
                 cache.put(e.request, newResponse)
               }
