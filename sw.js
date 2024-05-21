894f994c71fd5d318e50f466eb26f3789f322e5f
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2024-05-20 21:37:36 -0300

    Cache Dinamico Inmutable Static

--- sw.js
+++ sw.js
@@ -1,23 +1,58 @@
+// const CACHE_NAME = 'cache-1'
+
+const CACHE_STATIC_NAME = 'static-v1'
+const CACHE_DYNAMIC_NAME = 'dynamic-v1'
+
+const CACHE_INMUTABLE_NAME = 'inmutable-v1'
+
 self.addEventListener('install', e => {
-    const cacheProm = caches.open('cache-1').then(cache => {
+    const cacheProm = caches.open(CACHE_STATIC_NAME).then(cache => {
         return cache.addAll([
             '/',
             '/index.html',
             '/css/style.css',
             '/img/image.png',
             '/js/app.js',
-            'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
             '/pages/offline.html'
         ]).catch(error => {
             console.error('Failed to cache:', error);
             throw error;
         });
     });
-    e.waitUntil(cacheProm);
+    const cacheInputable = caches.open(CACHE_INMUTABLE_NAME).then(cache => {
+      return cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css')
+    });
+    e.waitUntil(Promise.all([cacheProm,cacheInputable]));
 });
 
-self.addEventListener('fetch',()=>{
+self.addEventListener('fetch',e=>{
+
+    // 2- Cache with Network Fallback
+    const respuesta = caches.match( e.request )
+      .then( res =>{
+
+        //si la respuesta existe retornamos la respuesta que es del cache
+        if(res)return res
+
+        // No existe el archivo
+        // tengo que ir a la web
+      
+        console.log('No existe', e.request)
+        
+        return fetch(e.request)
+          .then(newResponse =>{
+            caches.open(CACHE_DYNAMIC_NAME).then( cache =>{
+              if(cache){
+                cache.put(e.request, newResponse)
+              }
+
+            })
+            return newResponse.clone()
+          })
+      })
+
+    e.respondWith(respuesta)
     // 1 - Cache Only 
     // no va a haber petion que sale a la web
-    e.respondWith( caches.match( e.request ) )
+    // e.respondWith( caches.match( e.request ) )
 })
